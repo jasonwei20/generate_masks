d7b88f6781d456da13809d511dd886c789e0b570
jasonwengwei <jason.weng.wei@gmail.com>
2018-01-10 00:30:12 -0500

    Add everything.

---
+++ generate_masks.py
@@ -0,0 +1,99 @@
+################################## IMPORTS ##################################
+
+import os
+import sys
+import random
+import math
+import numpy as np
+import skimage.io
+import matplotlib
+import matplotlib.pyplot as plt
+
+import coco
+import utils
+import model as modellib
+import visualize
+
+#%matplotlib inline
+
+# Root directory of the project
+ROOT_DIR = os.getcwd()
+
+# Directory to save logs and trained model
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+
+# Local path to trained weights file
+COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+# Download COCO trained weights from Releases if needed
+if not os.path.exists(COCO_MODEL_PATH):
+    utils.download_trained_weights(COCO_MODEL_PATH)
+
+# Directory of images to run detection on
+IMAGE_DIR = os.path.join(ROOT_DIR, "images")
+
+################################## CONFIGURATIONS ##################################
+
+class InferenceConfig(coco.CocoConfig):
+    # Set batch size to 1 since we'll be running inference on
+    # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+config = InferenceConfig()
+config.display()
+
+################################## CREATE MODEL, LOAD TRAINED WEIGHTS ##################################
+
+# Create model object in inference mode.
+model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
+
+# Load weights trained on MS-COCO
+model.load_weights(COCO_MODEL_PATH, by_name=True)
+
+# COCO Class names
+# Index of the class in the list is its ID. For example, to get ID of
+# the teddy bear class, use: class_names.index('teddy bear')
+class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+               'bus', 'train', 'truck', 'boat', 'traffic light',
+               'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
+               'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+               'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
+               'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+               'kite', 'baseball bat', 'baseball glove', 'skateboard',
+               'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+               'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+               'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+               'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+               'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+               'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+               'teddy bear', 'hair drier', 'toothbrush']
+
+################################## RUN MODEL ##################################
+
+# Load a random image from the images folder
+file_names = next(os.walk(IMAGE_DIR))[2]
+if '.DS_Store' in file_names:
+    file_names.remove('.DS_Store')
+
+# Jason's edits
+for filename in file_names:
+    image = skimage.io.imread(os.path.join(IMAGE_DIR, filename))
+    results = model.detect([image], verbose=1)
+    r = results[0]
+
+    masks = r['masks']
+    print()
+    print("Mask for", filename)
+    print("Number of objects:", masks.shape[2])
+    print("Dimensions of picture:", masks.shape[0], "x", masks.shape[1])
+    print()
+
+    output_file_name = "output_masks/mask_" + filename
+    output_file_name = output_file_name[:-4]
+    np.save(output_file_name, masks)
+    print("Masked saved in", output_file_name)
+
+    #visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'], class_names, r['scores'])
+
+# mask = np.load('output/masks_out_test.npy') #load the image back in
